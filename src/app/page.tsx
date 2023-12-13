cf356e22e7498619840d14c20cf3fddeb7743864
Gath1 <103825298+Gath1@users.noreply.github.com>
2023-12-13 15:02:08 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
               <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">Gathan Al Dhafa</h2>
               <h3 className="text-2xl font-bold py-2 md:text-3xl">Front-End Developer</h3>
               <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto">
-                 <span className="font-bold">I`m Gathan Al Dhafa</span>, a <span className="font-bold">Front-End Developer </span>from <span className="font-bold">Binus University. </span> 
+                 <span className="font-bold">I`m Gathan Al Dhafa</span>, a <span className="font-bold">Full Stack Developer </span>from <span className="font-bold">Binus University. </span> 
                  Dive into my work to see the seamless blend of design and technology, showcasing my passion for crafting user-centric web experiences. 
                  Explore the intersection of creativity and functionality with me!
               </p>
